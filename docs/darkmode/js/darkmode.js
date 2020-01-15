5f6dfb84f4e135a9c5d80d2037263c46072b065c
jensnti <jens.andreasson@ga.ntig.se>
2020-01-15 15:00:12 +0100

    added darkmode basic

---
+++ docs/darkmode/js/darkmode.js
@@ -0,0 +1,23 @@
+/*
+ * Vänta på att sidan har laddats.
+ * Läs mer, https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
+ */
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+     
+    let sw = document.querySelector('#switch');
+    let navbar = document.querySelector('.navbar');
+    let darkmode = false;
+
+    sw.addEventListener('click', (e) => {
+        
+        darkmode = sw.checked === false ? darkmode = false : darkmode = true;
+        console.log(darkmode)
+        if(darkmode === true) {
+            navbar.classList.toggle('dark');
+        } else {
+            navbar.classList.toggle('dark');
+        }
+    });
+
+});
