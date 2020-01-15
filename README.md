b02be49a9bf8f61efb1d0972fbeec37341f21ccf
jensnti <jens.andreasson@ga.ntig.se>
2020-01-15 13:40:00 +0100

    uppdaterade en del instruktioner + knappar

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ## Efterfrågade övningar och exempel.
 
+Du har fått tillgång till detta repo genom Github classroom. Det är en template
+så den ska finnas på ditt github konto.
+Om det finns där så behöver du sedan klona det lokalt.
+
+    git clone URL
+    cd REPONAME
+
 Naviera till https://jensnti.github.io/wu-januari/ och arbeta med valfri del.
 
 * Studera koden och de olika delarna.
---
+++ docs/knappar/js/knappar.js
@@ -0,0 +1,27 @@
+/*
+ * Vänta på att sidan har laddats.
+ * Läs mer, https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
+ */
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+    
+    // Hitta knappen i DOM-trädet och spara den i variabeln button.
+    // Ett tips är att kunna och känna till vilka olika selectors vi kan använda.
+    // Vad är ens en selector?
+    // https://developer.mozilla.org/en-US/docs/Glossary/CSS_Selector
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelector
+    let button = document.querySelector('#js-button');
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log("Button clicked");
+        console.log(e);
+    });
+
+    let aBtn = document.querySelector('#a-button');
+
+    aBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log("Link clicked");
+    });
+});
